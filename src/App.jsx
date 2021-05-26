3753012c156ea3e3fa888db68e25dcae3c208de3
bradygrapentine <brady.grapentine@gmail.com>
2021-05-26 12:20:21 -0400

    play again button working

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,43 @@ export class App extends Component {
     state: 'new',
     mines: 10,
   }
+
+  handleCellClick = async (row, column) => {
+    const body = { row: row, col: column }
+    const response = await fetch(
+      `https://minesweeper-api.herokuapp.com/games/${this.state.id}/check`,
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(body),
+      }
+    )
+    if (response.status === 200) {
+      // Get the response as JSON
+      const game = await response.json()
+      // Make that the new state!
+      this.setState(game)
+    }
+  }
+
+  handleNewGame = async () => {
+    // Make a POST request to ask for a new game
+    const response = await fetch(
+      'https://minesweeper-api.herokuapp.com/games',
+      {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+      }
+    )
+    if (response.status === 200) {
+      // Get the response as JSON
+      const game = await response.json()
+      console.log(game)
+      // Make that the new state!
+      this.setState(game)
+    }
+  }
+
   render() {
     return (
       <>
@@ -26,7 +63,12 @@ export class App extends Component {
               <article>Count: Test</article>
             </li>
             <li>
-              <button>Play Again</button>
+              <button
+                className="playAgain"
+                onClick={() => this.handleNewGame()}
+              >
+                Play Again
+              </button>
             </li>
           </ul>
           <div className="boardUI">
@@ -35,7 +77,14 @@ export class App extends Component {
               return (
                 <div className="row">
                   {row.map((cell, columnIdx) => {
-                    return <button className="cell">{cell}</button>
+                    return (
+                      <button
+                        className="cell"
+                        onClick={() => this.handleCellClick(rowIdx, columnIdx)}
+                      >
+                        {cell}
+                      </button>
+                    )
                   })}
                 </div>
               )
